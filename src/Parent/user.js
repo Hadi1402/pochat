94c1a493ffac8b4c693baa51a3412ada542e1c2f
Hadi1402 <hadisaedi470@gmail.com>
2023-05-12 19:51:42 +0330

    Continuation componnet user

--- src/Parent/user.js
+++ src/Parent/user.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useState,useRef} from 'react';
 import "../static/css/chat.css";
 import {Avatar} from "@material-ui/core";
 import { SearchOutlined } from "@material-ui/icons";
@@ -16,25 +16,30 @@ class User extends React.Component{
     this.status_Ref = React.createRef();
     this.check_Ref = React.createRef();
     this.state = {
-      status_user:"غعال",
-     // row_display:'block',
-      row_color:"#e4e4e4"
-
+     status_user:'فعال',
+     row_display:'block',
+    row_color:"#e4e4e4",
+     
     }
-  this.onChange = this.onChange.bind(this)
-    }
+   this.onChange = this.onChange.bind(this)
+   this.onClick = this.onClick.bind(this) 
+     }
+
+   onClick = (e) => {
+    const [checked, setChecked] = useState();
+      setChecked(e.target.value);
+    };
 
 
-    onChange(e){
-      console.log("ddddddddddddd");
-      const check=this.check_Ref.current.checked
-      var checkbox = document.getElementsByClassName('check_box');
-      var  td=document.getElementsByClassName('td_status').innerHTML
-      if(checkbox.checked == true)
-      {
-          td=this.setState({status_user:'غیر فعال'})
-         // this.setState({row_display:'none'})
-      }
+
+    onChange = (e) => {
+     const check=this.check_Ref.current;
+     //var checkbox = document.getElementsByClassName('check_box');
+     // var  td=document.getElementsByClassName('td_status').innerHTML
+       //const [status_user , setstatus_user]=useState('');
+         if(check)
+         this.setState({status_user:'غیر فعال'})
+         // this.setState({row_display:'none'}
     }
 
 
@@ -44,9 +49,6 @@ class User extends React.Component{
     return(
 
       <div>     
-        
-       
-     
         <div className="div_user">
           <div className="search_user">
             <SearchOutlined />
@@ -59,7 +61,7 @@ class User extends React.Component{
                 <th className="td_user_table" colspan="2"> نام کاربری </th>
                 <th className="td_user_table" colspan="2">  ایمیل </th>
                 <th className="td_user_table" colspan="2">  تاریخ ایجاد </th>
-                <th className="td_user_table" colspan="2">  وضعیت </th> 
+                <th className="td_user_table" colspan="2">  وضعیت کاربر </th> 
                 <th className="td_user_table" colspan="2">  انتخاب</th>
 
             </tr>
@@ -73,8 +75,13 @@ class User extends React.Component{
                 <td  colspan="2"> <img  className="img_user" src={u.عکس}/> <span> {u["نام کاربری "]} </span> </td>  
                 <td  colspan="2"> {u["ایمیل "]} </td>  
                 <td  colspan="2">  {u["تاریخ ایجاد"]} </td> 
-                <td ref={this.status_Ref} className="td_status" colspan="2">  {this.state.status_user} </td>
-                <td ref={this.check_Ref} className="check_box" colspan="2">  <input type="checkbox" /> </td>  
+                <td ref={this.status_Ref} className="td_status" colspan="2"> {this.state.status_user}  </td>
+                <td  className="check_box" colspan="2">  <input 
+                     ref={this.check_Ref}
+                     type="checkbox" 
+                     onClick={onclick}
+                     />
+                </td>  
               </tr>
             </tbody>
        
