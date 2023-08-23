4c9bc0b3ebf57f700ef555d226850b301b93faf8
unknown <amirkiller1387@gmil.com>
2023-08-23 10:28:12 +0330

    some other designs added

--- src/Component/pochat.js
+++ src/Component/pochat.js
@@ -6,6 +6,7 @@ import "../static/css/varriables.css";
 import logo from "../static/icons/logo porya-01.svg"
 import menu_logo from "../static/icons/logo.svg"
 import background_img from "../static/img/home_page_background.png"
+import { Divider } from '@material-ui/core';
 // import {Link, NavLink} from "react-router-dom";
 // import HomeSideBar from "../Parent/home-sidebar";
 // import Animate from 'animate.css';
@@ -14,98 +15,127 @@ import background_img from "../static/img/home_page_background.png"
 class Home extends Component {
   constructor(props) {
     super(props);
+    // ///////////////////////////////////// state
     this.state = {
       scrollTop: 0,
       screenSize:{
-        width: 0,
-        height: 0
+        width: window.innerWidth,
+        height: window.innerHeight
       },
       mousePos: {
         x: 0,
         y: 0
       },
-      displayHeight: 10,
-      displayWidth: 10,
     };
+    // ///////////////////////////////////// state
     this.handleScroll = this.handleScroll.bind(this);
     this.onMouseMove = this.onMouseMove.bind(this);
     this.onWindowSizeChanged = this.onWindowSizeChanged.bind(this);
-    window.addEventListener('resize', this.onWindowSizeChanged);
-  }
+    this.handleContextMenu = this.handleContextMenu.bind(this);
 
+    window.addEventListener('resize', this.onWindowSizeChanged);
+
+    this.content1Ref = React.createRef();
+    this.content2Ref = React.createRef();
+    this.content3Ref = React.createRef();
+    this.content4Ref = React.createRef();
+
+  }
   componentDidMount() {
     window.addEventListener('scroll', this.handleScroll);
     window.addEventListener('mousemove', this.onMouseMove);
     window.addEventListener('resize', this.onWindowSizeChanged);
+    document.addEventListener('contextmenu', this.handleContextMenu);
 
-    console.log("here is didmount")
     this.onWindowSizeChanged()
   
   }
-
   componentWillUnmount() {
     window.removeEventListener('scroll', this.handleScroll);
     window.removeEventListener('mousemove', this.onMouseMove);
     window.removeEventListener('resize', this.onWindowSizeChanged);
-    
+    document.removeEventListener('contextmenu', this.handleContextMenu);
+
   }
+  handleContextMenu(event) {// for block right click
+    event.preventDefault();
+    console.log('Right-Click event was blocked');
+  }
+  handleClickToScroll(num) {
+    const content1 = document.querySelector('#content1');
+    const content2 = document.querySelector('#content2');
+    const content3 = document.querySelector('#content3');
+    const content4 = document.querySelector('#content4');
 
-  handleClickToScroll() {
-    console.log("clicked")
+    var target = 0
 
-    const content = document.querySelector('#content');
-
+    if ( num === 1 ){
+      var target = content1
+    } else if ( num === 2){
+      var target = content2
+    } else if ( num === 3){
+      var target = content3
+    } else if ( num === 4){
+      var target = content4
+    } 
+    
     window.scrollTo({
-      top: content.offsetTop, 
-      left: content.offsetLeft, 
+      top: target.offsetTop, 
+      left: target.offsetLeft, 
       behavior: 'smooth' 
     });
-
   }
-    
   onWindowSizeChanged() {
-    console.log("شروع فانکشن")
-    console.log(this.state.displayHeight)
-    console.log(this.state.displayWidth)
-    const displayWidth = window.innerWidth || window.clientWidth;
-    const displayHeight = window.innerHeight || window.clientHeight;
+    var displayWidth = window.innerWidth || window.clientWidth;
+    var displayHeight = window.innerHeight || window.clientHeight;
     
-    console.log("اینجا اندازه ی صفحه رو پیدا میکنیم")
-    console.log(this.state.displayHeight)
-    console.log(this.state.displayWidth)
     this.setState({
-      displayWidth,
-      displayHeight,
+      screenSize: {
+        width: displayWidth,
+        height: displayHeight
+      }
     });
     
-    console.log("اینجا اندازه رو به استیت منتقل میکنیم")
-    console.log(this.state.displayHeight)
-    console.log(this.state.displayWidth)
-    this.onHeightOfHomePage()
-    console.log("اینجا از استیت اندازه رو روی المنت اعمال میکنیم")
-    console.log(this.state.displayHeight)
-    console.log(this.state.displayWidth)
+    this.onSizeOfHomePage()
+    
+    this.setState({
+      screenSize: {
+        width: displayWidth,
+        height: displayHeight
+      }
+    });
   }
-
-  onHeightOfHomePage() {
+  onSizeOfHomePage() {
     const home = document.querySelector('.home');
     const header = document.querySelector('#head_of_home_page');
+    const body = document.querySelector('#body_of_home_page');
+    const footer = document.querySelector('#footer_of_home_page');
+    const contents = document.querySelector('.content_of_home_page');
+    const content1 = document.querySelector('#content1');
+    const content2 = document.querySelector('#content2');
+    const content3 = document.querySelector('#content3');
+    const content4 = document.querySelector('#content4');
     
 
-    home.style.height = this.state.displayHeight * 6 + 'px';
-    header.style.height = this.state.displayHeight + 'px';
+    home.style.height = this.state.screenSize.height * 6 + 'px';
+    header.style.height = this.state.screenSize.height + 'px';
+    body.style.height = this.state.screenSize.height * 4 + 'px';
+    footer.style.height = this.state.screenSize.height + 'px';
+    contents.style.height = this.state.screenSize.height + 'px';
+    content1.style.height = this.state.screenSize.height + 'px';
+    content2.style.height = this.state.screenSize.height + 'px';
+    content3.style.height = this.state.screenSize.height + 'px';
+    content4.style.height = this.state.screenSize.height + 'px';
   }
-
   onMouseMove(event) {
     // console.log("your mouse position is : " , "x :",this.state.mousePos.x," y : ",this.state.mousePos.y)
     const mousePos = { ...this.state.mousePos, x: event.clientX, y: event.clientY };
     this.setState({ mousePos });
   }
-
-
   handleScroll(event) {
     const scrollTop = window.scrollY;
-    this.setState({ scrollTop });
+
+    this.setState({ scrollTop:scrollTop });
   
     if (scrollTop > 150) {
       this.handleHeaderHide();
@@ -115,7 +145,6 @@ class Home extends Component {
       this.handleHeaderShow();
     }
   }
-
   handleHeaderHide() {
     // console.log("reached to more than 150");
   
@@ -131,8 +160,7 @@ class Home extends Component {
 
     
     
-  }
-  
+  } 
   handleHeaderShow() {
     // console.log("reached to less than 150");
     
@@ -150,16 +178,9 @@ class Home extends Component {
     topBar.style.marginTop = '-150px'  
 
   }
-
-
   render() {
-
-    const { mousePos, width, height, displayWidth, displayHeight } = this.state;
-
-
     return (
       <div className="home" ref={this.myElementRef}>
-        
         <header id='head_of_home_page'>
           <header id='home_page_header'>
 
@@ -167,8 +188,8 @@ class Home extends Component {
               <h5> position from top is : {this.state.scrollTop}</h5>
               <h5> position mouse x is : {this.state.mousePos.x}</h5>
               <h5> position mouse y is : {this.state.mousePos.y}</h5>
-              <h5> width of your window is : {this.state.displayWidth}</h5>
-              <h5> height of your window is : {this.state.displayHeight}</h5>
+              <h5> width of your window is : {this.state.screenSize.width}</h5>
+              <h5> height of your window is : {this.state.screenSize.height}</h5>
             </div>
 
             <div id='home_page_top_bar'>
@@ -185,41 +206,59 @@ class Home extends Component {
             <button id="home_page_menu_button"><img src={menu_logo}/></button>
 
             <div id="menu">
-              <div className="menu_item" id="item_1" onClick={this.handleClickToScroll}>menu_1</div>
-              <div className="menu_item" id="item_2" onClick={this.onWindowSizeChanged}>menu_2</div>
-              <div className="menu_item" id="item_3">menu_3</div>
-              <div className="menu_item" id="item_4">menu_4</div>
+              <div className="menu_item" id="item_1" onClick={() => this.handleClickToScroll(1)}>content 1</div>
+              <div className="menu_item" id="item_2" onClick={() => this.handleClickToScroll(2)}>content 2</div>
+              <div className="menu_item" id="item_3" onClick={() => this.handleClickToScroll(3)}>content 3</div>
+              <div className="menu_item" id="item_3" onClick={() => this.handleClickToScroll(4)}>content 4</div>
             </div>
-
+ 
           </header>
         </header>
-
-        <tbody id='body_of_home_page'>
-          <h1>this is tbody</h1>
-          <h1>this is tbody</h1>
-          <h1>this is tbody</h1>
-          <h1>this is tbody</h1>
-        </tbody>
-
-
-        <div id='content'>
-          <h1>content</h1>
-          <h1>content</h1>
-          <h1>content</h1>
-          <h1>content</h1>
-          <h1>content</h1>
-          <h1>content</h1>
-          <h1>content</h1>
-          <h1>content</h1>
-          <h1>content</h1>
-          <h1>content</h1>
-          <h1>content</h1>
-          <h1>content</h1>
-          <h1>content</h1>
-          <h1>content</h1>
-          <h1>content</h1>
-          <h1>content</h1>
-          <h1>content</h1>
+        <div id='body_of_home_page'>
+          <div id='content1' className='content_of_home_page'>
+            <div className='content_container'>
+              <div id='content1_pic'>
+              </div>
+              <div id='content1_text'>
+                <h1>پوچت چیست ؟</h1>
+              </div>
+            </div>
+          </div>
+          <div id='content2' className='content_of_home_page'>
+            <div className='content_container'>
+              <div id='content2_pic'>
+              </div>
+              <div id='content2_text'>
+                <h1>چرا پوچت ؟</h1>
+              </div>
+            </div>
+          </div>
+          <div id='content3' className='content_of_home_page'>
+            <div className='content_container'>
+              <div id='content3_pic'>
+              </div>
+              <div id='content3_text'>
+                <h1>ما برای شما چه امکاناتی داریم ؟</h1>
+              </div>
+            </div>
+          </div>
+          <div id='content4' className='content_of_home_page'>
+          <div className='content_container'>
+            <div id='content4_pic'>
+              </div>
+              <div id='content4_text'>
+                <h1>از چه نظری پوت بهترین است ؟</h1>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div id='footer_of_home_page'>
+          <div id='footer_container'>
+            <div id='footer_design'></div>
+            <div id='footer_content'>
+              <pre> ا هم میتونین در کمتر از 10 دقیقه از پوچت برای کاربرانتان استفاده کنید</pre>
+            </div>
+          </div>
         </div>
 
       </div>
