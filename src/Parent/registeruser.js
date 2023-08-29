b608284a86c62da3a126f3ceeab1f7496f9fcaac
unknown <amirkiller1387@gmil.com>
2023-08-30 00:50:55 +0330

    working on register page

--- src/Parent/registeruser.js
+++ src/Parent/registeruser.js
@@ -34,23 +34,60 @@ class RegisterUserPage extends Component {
      console.log("here1111111111111111111111111");
         
    }
+    onInputChange(witch){
+      const span1 = document.querySelector('#input_span_1');
+      const span2 = document.querySelector('#input_span_2');
+      const username = document.querySelector('#login_form_input_username').value;
+      const password = document.querySelector('#login_form_input_password').value;
+      
+      if (witch == 1){
+        console.log('1');
+        if (username != ''){
+            span1.style.top = 49 + 'px'
+            span1.style.fontSize = 0.6 + 'em'
+            
+        } else if (username == ''){
+            span1.style.top = 27.5 + 'px'
+            span1.style.fontSize = 1 + 'em'
+        }
+      }else if (witch == 2){
+        console.log('2')
+        if (password != ''){
+            span2.style.top = 49 + 'px'
+            span2.style.fontSize = 0.6 + 'em'
+            
+        } else if (password == ''){
+            span2.style.top = 27.5 + 'px'
+            span2.style.fontSize = 1 + 'em'
+        }
+      }
+    }
   
 
 
 render() {
   return(
     <div className="registerform">
-      <form class="register_form">
-        <p class="register_form_title">برای عضویت,فرم زیر را پر کنید</p>
-        <p class="register_form_message">ثبت نام کنید تا از قابلیت های بیشتر بهره مند شوید</p>
+      <div id="registerformbackgroundblur">
+        <form class="register_form">
+          <p class="register_form_title">ثبت نام </p>
+          <p class="register_form_message">برای ثبت نام لازمه فرم زیر رو پر کنید</p>
+          
           <label>
               <input 
               required="" 
               type="text" 
+              id='register_form_input_code'
               class="register_form_input"
               name="code"  
-              placeholder={'کد دعوت '} 
-              ref={this.code}/>
+              // placeholder={'کد دعوت '} 
+              ref={this.code}
+              onChange={e => {
+                // setUserName(e.target.value);
+                this.onInputChange(1);
+              }} 
+              // onFocus={inputChange(1)}
+              />
               <span>کد دعوت </span>
           </label>
 
@@ -64,46 +101,51 @@ render() {
               ref={this.username}/>
               <span>نام کاربری</span>
           </label>
-        <label>
-            <input 
-            required="" 
-            type="email" 
-            class="register_form_input"
-            name="email"  
-            placeholder={'email'} 
-            ref={this.email}/>
-            <span>ایمیل</span>
-        </label> 
-            
-        <label>
-            <input 
-            required="" 
-            type="password" 
-            class="register_form_input"
-            id="pass"
-            name="password"  
-            placeholder={'pass'}
-            // value={"abc"}
-            ref={this.password}/>
-            <span>کلمه عبور</span>
-        </label>
-        <label>
-            <input 
-            required="" 
-            type="password" 
-            class="register_form_input"
-            id="rePass"
-            name="repassword"  
-            placeholder={'repass'} 
-            ref={this.repassword}
-            // value={"abc"}
-            // onChange={notSame()}
-            />
-            <span>تکرار کلمه عبور</span>
-        </label>
-        <button class="register_form_submit" value = 'عضویت' onClick={this.onHandleRegistration} >Submit</button>
-        <p class="register_form_signin">قبلاً ثبت نام کرده ام <NavLink exact to={'/login'}> ورود </NavLink></p>
-    </form>
+
+          <label>
+              <input 
+              required="" 
+              type="email" 
+              class="register_form_input"
+              name="email"  
+              placeholder={'email'} 
+              ref={this.email}/>
+              <span>ایمیل</span>
+          </label> 
+
+          <label>
+              <input 
+              required="" 
+              type="password" 
+              class="register_form_input"
+              id="pass"
+              name="password"  
+              placeholder={'pass'}
+              // value={"abc"}
+              ref={this.password}/>
+              <span>کلمه عبور</span>
+          </label>
+
+          <label>
+              <input 
+              required="" 
+              type="password" 
+              class="register_form_input"
+              id="rePass"
+              name="repassword"  
+              placeholder={'repass'} 
+              ref={this.repassword}
+              // value={"abc"}
+              // onChange={notSame()}
+              />
+              <span>تکرار کلمه عبور</span>
+          </label>
+
+          <button class="register_form_submit" value = 'عضویت' onClick={this.onHandleRegistration} >Submit</button>
+          <p class="register_form_signin">قبلاً ثبت نام کرده ام <NavLink exact to={'/login'}> ورود </NavLink></p>
+        </form>
+      </div>
+
     </div>
   )
 
